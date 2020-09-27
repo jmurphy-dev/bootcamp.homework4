0160c9ff7a45f62d40f9c422c6bcb3df45924498
Jack Murphy <signal.space@protonmail.com>
2020-09-27 14:34:50 -0500

    Psudocode done

---
+++ script.js
@@ -0,0 +1,44 @@
+// Quiz Game
+
+    // Pregame
+
+        // Draw Rules of game
+
+        // Draw clock
+
+        // Optional** Draw current high score
+
+        // Draw button
+
+            // When the use presses the button start the game loop
+
+    
+    // Game Loop
+        
+        // Draw questions & start clock
+
+        // Store number of questions left
+
+        // Event loop
+
+            // On user choice
+
+                // Check answer & draw new questions & calculate new time & decrement questions left
+
+                    // If there are no more questions
+                        
+                        // Stop the clock & save the value of time remaining
+
+                        // Start the end game loop
+    
+    // End Game
+
+        // Draw game score & input field for player 
+        
+        // Add play result to list of player result
+
+        // Draw current high score && new game button
+
+            // When the user presses the button start the pregame loop
+
+
